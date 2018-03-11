4021295dc9a761ae1837b4a4ee193352cc8adc50
Jingl3s <Jingl3s@yopmail.com>
2018-03-10 23:26:52 -0500

    Add more services configuration Update on menu action on push button added

--- README.md
+++ README.md
@@ -2,6 +2,46 @@
 Display information on OLED display connected to Orange Pi Zero
 
 
+# Installation
+```shell
+git clone https://github.com/jingl3s/OrangePiZero_OLED_Dht_display.git
+
+```
+
+* Setup the informations needed
+  * Edit the disp_oled.py
+    * width, height, and number_line_per_page of oled screen
+    * get_oled_device() if reset pin
+    * create_dht() for your dht sensor
+    * dict_pin in case of push button connected
+    * page_lines settings of information displayed
+    * WAIT_TIME time waiting between each page displayed
+    
+* Service lancé au démarrage<br> 
+
+```shell
+sudo nano /lib/systemd/system/opi_oled_dht.service
+```
+
+```ini
+[Unit]
+Description=Sound Interactions
+After=multi-user.target
+[Service]
+Type=idle
+ExecStart=/usr/bin/python3 /home/__TBD__/OrangePiZero_OLED_Dht_display/disp_oled.py
+[Install]
+WantedBy=multi-user.target
+``` 
+
+__TBD__ is defined path on your installation
+
+```shell
+sudo chmod 644 /lib/systemd/system/opi_oled_dht.service
+sudo systemctl daemon-reload
+sudo systemctl enable opi_oled_dht.service
+```
+
 
 
 # Links
@@ -12,3 +52,19 @@ Display information on OLED display connected to Orange Pi Zero
 - GIT submodule
   - https://stackoverflow.com/questions/7597748/linking-a-single-file-from-another-git-repository
   git submodule add <URL> <local_link_name>
+  
+- ETC configuration 
+  - https://github.com/laneboysrc/rc-headless-transmitter/blob/master/configurator/orangepizero/INSTALL.md
+  
+  
+## Meta
+
+Jingl3s
+
+Distribué sous la licence BSD. Voir ``LICENSE`` pour plus d'information.
+
+
+## Contribution
+
+Fork du projet
+  
--- disp_oled.py
+++ disp_oled.py
@@ -38,7 +38,7 @@ global height
 height = 64
 
 global col1
-col1 = 4
+col1 = 3
 
 global number_line_per_page
 number_line_per_page = 4
@@ -143,43 +143,61 @@ def date():
 
 def get_dht(dht_sensor):
     temp, hum = dht_sensor.get_valeurs()
-    return '{:0.1f}°  {:0.1f}%'.format(
+    sensor = dht_sensor.get_sensor()
+    return 'DHT{}: {:5.1f}°  {:4.1f}%'.format(sensor,
         temp, hum)
 
 
-def stats_page(device):
-    global dht11
-    global dht22
+def stats_page():
+    '''
+    Format the text to display and return the list of string to display
+    '''
     global page_lines
     global current_line
 
+    list_text = list()
+    # First start display a dedicated message
+    if looper == 0:
+        list_text.append('WELCOME TO OPi ZERO')
+        list_text.append('')
+        list_text.append('Starting up...')
+    elif looper > 0:
+        # Format the displayed information per pages by calling each function returning text to display
+        for _ in range(number_line_per_page):
+            
+            text_to_add = ''
+            if current_line < len(page_lines):
+                if not page_lines[current_line][0] is None:
+                    if page_lines[current_line][1] is not None:
+                        text_to_add = page_lines[current_line][0](
+                            page_lines[current_line][1])
+                    else:
+                        text_to_add = page_lines[current_line][0]()
+            list_text.append(text_to_add)
+            current_line = (current_line + 1) % len(page_lines)
+    return list_text
+
+
+def disp_text(device, p_list_text):
+    '''
+    Display the content of list to the device 
+    :param device: Luma device
+    :param p_list_text: List of text with a string per line to display
+    '''
     with canvas(device) as draw:
         draw.rectangle((0, 0, 127, height - 1), outline="white", fill="black")
-        if looper == 0:
-            draw.text((col1, line_pixel[0]), 'WELCOME TO OPi ZERO',
+
+        for line, text in enumerate(p_list_text):
+            draw.text((col1, line_pixel[line]), text,
                       font=font10, fill=255)
-            draw.text((col1, line_pixel[3]),
-                      'Starting up...', font=font10, fill=255)
-        elif looper > 0:
-
-            for ligne_en_cours in range(number_line_per_page):
-
-                if current_line < len(page_lines):
-                    if page_lines[current_line][1] is not None:
-                        draw.text((col1, line_pixel[ligne_en_cours]), page_lines[current_line][0](
-                            page_lines[current_line][1]),  font=font10, fill=255)
-                    else:
-                        draw.text((col1, line_pixel[ligne_en_cours]),
-                                  page_lines[current_line][0](),  font=font10, fill=255)
-                else:
-                    draw.text((col1, line_pixel[ligne_en_cours]),
-                              '',  font=font10, fill=255)
-                current_line = (current_line + 1)
-            if current_line >= len(page_lines):
-                current_line = 0
 
 
 def get_oled_device():
+    '''
+    Intialize my dedicated reset of device to start properly on I2C
+    
+    :return device: Luma device
+    '''
     from luma.core.interface.serial import i2c  # @UnresolvedImport
     from luma.oled.device import ssd1306  # @UnresolvedImport
 
@@ -207,6 +225,10 @@ def get_oled_device():
 
 
 def get_my_device():
+    '''
+    Try getting physical device otherwize return the emulated one
+    :return device as luma
+    '''
     try:
         dev = get_oled_device()
     except:
@@ -215,6 +237,10 @@ def get_my_device():
 
 
 def get_emul_device():
+    '''
+    Create a luma emulator device
+    :return device as luma
+    '''
     from luma.emulator.device import pygame  # @UnresolvedImport
     device = pygame(mode="1")
     # Activer la ligne suivante permet de bloquer l'affichage sur le dernier text lors de l'arret du programme
@@ -226,16 +252,19 @@ def get_push_button(p_dict_pin):
     disp = list()
     for pin, pin_info in p_dict_pin.items():
         disp.append("{} {}".format(pin, pin_info["last_state"]))
-    return "INT: " + ",".join(disp)
+    return "Push But: " + ",".join(disp)
 
 
 class DHTFake(object):
-    def __init__(self):
-        pass
+    def __init__(self, sensor):
+        self._sensor = sensor
 
     def get_valeurs(self):
         self.valeurs = (randint(-200, 300) / 10.0, randint(100, 950) / 10.0)
         return self.valeurs
+    
+    def get_sensor(self):
+        return self._sensor
 
 
 def create_dht():
@@ -246,17 +275,115 @@ def create_dht():
         dht11 = DHTInterface(0, 11)
         dht22 = DHTInterface(1, 22)
     except:
-        dht11 = DHTFake()
-        dht22 = DHTFake()
+        dht11 = DHTFake(11)
+        dht22 = DHTFake(22)
 
 
-def call_shutdown(pin_info):
+def menu(lmenu, device=None, sous_menu=False, gpio_pins=None):
+    '''
+    Display a menu to the user and add a dedicated character in order to identify the current activated menu
+    
+    The device is used as parameter to display the menu content
+    
+    
+    :param lmenu: List of menu to display
+    :param device: Luma device for display
+    :param sous_menu: If True, the sub menu to confirm is not displayed otherwise always disabled
+    :param gpio_pins: dictionary of GPIO pins to use 
+    '''
 
-    if pin_info["count"] == 3:
+    # Copy the menu as modified to display the prompt
+    smenu = lmenu[:]
+    sel = 0
+    eval_ret = None
+    
+    # Wait at least a selection in menu is confirmed
+    while sel < len(lmenu) and eval_ret is None:
+        
+        # Add the user prompt to identify menu position
+        smenu[sel] = '>' + smenu[sel]
+        
+        disp_text(device, smenu)
+        
+        # attente de saisi utilisateur car menu demande
+        val_lu = action_utilisateur(gpio_pins)
+        
+        # Goes next position in the menu
+        if val_lu == True:
+            sel = (sel + 1) % len(smenu)
+        # Activate selection of menu
+        elif val_lu == False:
+            
+            print('Menu selectionne : {}'.format(smenu[sel][1:]))
+            
+            # Ajout d'une demamde de confirmation pour valider la selection'
+            if not sous_menu:
+                confirm = list()
+                confirm.append('annuler')
+                confirm.append('confirmer')
+
+                eval_ret1 = menu(confirm, device, True, gpio_pins=gpio_pins)
+            
+                # modification ret seulement si confirme
+                if eval_ret1 == 1:
+                    eval_ret = sel
+            else:
+                eval_ret = sel
+        # Restore default menu as copy
+        smenu = lmenu[:]
+    return eval_ret
+
+
+def action_push_button(pin_info, pins_complete):
+    '''
+    Function called when the GPIO change the current state in order to process something
+    
+    It will display a menu to user as the gpio button are use to go accros the menu and validate with only two buttons
+    :param pin_info: The current dictionary information of key pressed
+    :param pins_complete: All the dictionary of gpio configured
+    '''
+    global device
+#     if pin_info["count"] == 3:
+    if True:
         print("Extinction demandé {}".format(pin_info["count"]))
+
+        dmenu = dict()
+        dmenu['Eteindre'] = 'sudo halt'
+        dmenu['Test'] = 'print(\'test\')'
+        dmenu['Quitter menu'] = '0'
+        
+        lmenu = list(dmenu.keys())
+        
+        pin_info["count"] = 0
+        pin_info["change"] = False
+
+        index = menu(lmenu, device, gpio_pins=pins_complete)
+        if index in lmenu:
+            eval_ret = eval(dmenu[lmenu[index]])  # @UnusedVariable
+
         pin_info["count"] = 0
     else:
         print("Passage")
+
+
+def action_utilisateur(gpio_pin):
+    '''
+    Wait until a gpio changes in order to return the status change
+    Infinite loop as user action request
+
+    :param gpio_pin:
+    :return: True if first gpio key is pressed and False otherwise
+    '''
+    retour = None
+    cle_inter = list(gpio_pin.keys())
+
+    while retour is None:
+        io_verif_status(gpio_pin, desative_fonction=True)
+        for index, gpio_key in enumerate(cle_inter):
+            if gpio_pin[gpio_key]["change"]:
+                retour = not (index)
+        time.sleep(0.05)
+    return retour
 
 
 def io_setup(p_list_pin):
@@ -299,22 +426,30 @@ def io_setup(p_list_pin):
     return dict_pin
 
 
-def io_verif_status(p_dict_pin):
+def io_verif_status(p_dict_pin, desative_fonction=False):
+    '''
+    Detect the button press and call the associated function if any
+    :param p_dict_pin:
+    :param desative_fonction:
+    '''
     # Activation du reset pour avoir l'affichage fonctionnel
 
     for pin_info in p_dict_pin.values():
+        last_state = pin_info["last_state"]
         current = GPIO.input(pin_info["pin"])
 
-        if current and pin_info["last_state"] != current:
+        pin_info["last_state"] = current
+        
+        if current and last_state != current:
             print("Changement detecte")
             pin_info["change"] = True
             pin_info["count"] += 1
-            if pin_info["fonction"] is not None:
-                pin_info["fonction"](pin_info)
+            if not desative_fonction:
+                if pin_info["fonction"] is not None:
+                    pin_info["fonction"](pin_info, p_dict_pin)
         else:
             pin_info["change"] = False
 
-        pin_info["last_state"] = current
 
 
 def main():
@@ -324,8 +459,11 @@ def main():
     global current_line
     global number_line_per_page
     global font10
-    
-    temps_attente = 2
+    global dht11
+    global dht22
+    global device
+
+    WAIT_TIME = 1
     looper = 0
 
 # breakpoint
@@ -345,7 +483,7 @@ def main():
         p_list_pin.append(15)
         p_list_pin.append(16)
         dict_pin = io_setup(p_list_pin)
-        dict_pin[15]["fonction"] = call_shutdown
+        dict_pin[15]["fonction"] = action_push_button
         dict_pin[16]["fonction"] = None
     else:
         dict_pin = dict()
@@ -353,16 +491,18 @@ def main():
     current_line = 0
 
     page_lines = list()
-    page_lines.append((platform_info, None))
     page_lines.append((lan_ip, None))
     page_lines.append((uptime, None))
-    page_lines.append((date, None))
+    page_lines.append((None, None))
+    page_lines.append((None, None))
 
-    page_lines.append((cpu_usage, None))
+#     page_lines.append((platform_info, None))
+#     page_lines.append((date, None))
+#     page_lines.append((cpu_usage, None))
+#     page_lines.append((mem_usage, None))
+#     page_lines.append((disk_usage, '/'))
+
     page_lines.append((cpu_temperature, None))
-    page_lines.append((mem_usage, None))
-    page_lines.append((disk_usage, '/'))
-
     page_lines.append((get_dht, dht11))
     page_lines.append((get_dht, dht22))
     page_lines.append((get_push_button, dict_pin))
@@ -377,12 +517,13 @@ def main():
 
     while True:
         io_verif_status(dict_pin)
-        stats_page(device)
+        list_text = stats_page()
+        disp_text(device, list_text)
         if looper == 0:
-            time.sleep(temps_attente * 2)
+            time.sleep(WAIT_TIME * 2)
             looper = 1
         else:
-            time.sleep(temps_attente)
+            time.sleep(WAIT_TIME)
 
 
 if __name__ == "__main__":
